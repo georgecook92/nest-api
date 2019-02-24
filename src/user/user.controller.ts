4230de4d60d1358a35042bf60bc46dfc8f02cfa9
George <george.cook@redant.com>
2019-02-24 19:22:21 +0000

    add: create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
+import { CreateUserDTO } from './create-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -14,5 +15,10 @@ export class UserController {
   @Get(':id')
   fetchOne(@Param('id') id): Promise<User> {
     return this.userService.findById(id)
+  }
+
+  @Post()
+  create(@Body() user: CreateUserDTO) {
+    return this.userService.create(user)
   }
 }

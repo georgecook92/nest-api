ac61ec8e9b036da69052c43f8497ce577fb3b7ef
George <george.cook@redant.com>
2019-02-28 22:39:45 +0000

    add: login service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDTO } from './create-user.dto';
 import * as bcryptjs from 'bcryptjs'
+import { LoginDTO } from './login.dto';
 
 
 @Injectable()
@@ -27,7 +28,21 @@ export class UserService {
       password: this.hashPassword(user.password)
     })
   }
+  async login(dto: LoginDTO): Promise<string> {
+    const user: User = await this.userRepository.findOne({ email: dto.email })
+    if (!user) throw new UnauthorizedException()
+    const match = this.comparePassword(
+      dto.password,
+      user.password
+    )
+    if (!match) throw new UnauthorizedException()
+    return 'token'
+    
+  }
   hashPassword(password: string): string {
     return bcryptjs.hashSync(password, this.saltRounds)
+  }
+  comparePassword(password: string, hash: string): boolean {
+    return bcryptjs.compareSync(password, hash)
   }
 }
